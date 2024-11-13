7a9e06aa288c691293a55582dbd8af158cf38eac
MarinaMich <mar7995@yandex.ru>
2024-11-13 15:25:45 +0300

    Добавляет функционал переключения списков абониментов с помощью табов

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,23 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+// Табы в выборе абонимента
+
+const seasonTicketsTabs = document.querySelectorAll('.tabs__button');
+const seasonTicketsLists = document.querySelectorAll('.season-tickets__list');
+let currentTab;
+
+const thisTab = (index) => {
+  seasonTicketsTabs.forEach((tab) => tab.classList.remove('tabs__button--current'));
+  seasonTicketsLists.forEach((list) => list.classList.remove('season-tickets__list--current'));
+  seasonTicketsTabs[index].classList.add('tabs__button--current');
+  seasonTicketsLists[index].classList.add('season-tickets__list--current');
+};
+
+seasonTicketsTabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    currentTab = index;
+    thisTab(currentTab);
+  });
+});
