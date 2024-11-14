660cd0b1474f339416430814ab00867293174c8e
MarinaMich <mar7995@yandex.ru>
2024-11-14 17:07:41 +0300

    Добавляет iframe в блок о зале

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,25 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
+// Включение видеоролика
+
+const cover = document.querySelector('.visuals__cover');
+const video = document.querySelector('.visuals__iframe');
+const button = document.querySelector('.visuals__button');
+
+button.addEventListener('click', () => {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', 'https://www.youtube.com/watch?v=9TZXsZItgdw');
+  iframe.setAttribute('width', '320');
+  iframe.setAttribute('height', '170');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('loading', 'lazy');
+  video.appendChild(iframe);
+  cover.classList.add('visuals__cover--none');
+  document.getElementById('play').style.display = 'none';
+});
+
 // Табы в выборе абонимента
 
 const seasonTicketsTabs = document.querySelectorAll('.tabs__button');
