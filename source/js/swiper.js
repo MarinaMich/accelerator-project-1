db94f07a83a99164125aeeb763724d1a952e7104
MarinaMich <mar7995@yandex.ru>
2024-11-28 23:29:37 +0300

    редактирует настройки слайдеров, стилизует планшетную версию

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -32,36 +32,20 @@ const slides = new Swiper('.slides__container', {
   },
 });
 
-const slidesRev = new Swiper('.slides-rev__container', {
-  modules: [Navigation],
+const slidesRev = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
   slidesPerView: 1,
   loop: false,
-  /*grabCursor: true,
+  grabCursor: true,
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-  mousewheel: {
-    invert: true,
-  },*/
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 'auto',
-      spaceBetween: 40,
-    },
-    // when window width is >= 1366px
-    1366: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  },
 });
 
 slides.slideNext();
-slidesRev.slideNext();
+const swiper = document.querySelector('.slides-rev__containers').slidesRev;
